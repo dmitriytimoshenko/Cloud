06453abec49cd2d3a70841247be12a96fb98b9be
dmitriytimoshenko <asassinich@gmail.com>
2019-05-31 00:52:02 +0300

    редактирование persistence.xml, JPASplitterDAO

--- src/main/java/Splitter/DB/JPASplitterDAO.java
+++ src/main/java/Splitter/DB/JPASplitterDAO.java
@@ -1,4 +1,22 @@
 package Splitter.DB;
 
-public class JPASplitterDAO {
+import Splitter.DAO.SplitterDAO;
+import Splitter.entity.Splitter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository("JPAsplitterDAO")
+public class JPASplitterDAO implements SplitterDAO {
+
+    @PersistenceContext(unitName = "splitterPU")
+    private EntityManager em;
+
+    @Transactional
+    public void addSplitter(Splitter splitter) {
+        em.persist(splitter);
+    }
 }
